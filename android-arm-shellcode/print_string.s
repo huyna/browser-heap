984a3b92579f977e0a4cb9347c51f78f4cd0cc20
huyna <huyna89@hotmail.com>
2015-08-22 17:01:34 +0700

    Create print_string.s

---
+++ android-arm-shellcode/print_string.s
@@ -0,0 +1,25 @@
+.section .text
+.global _start
+
+_start:
+
+    .code 32
+    # Thumb-Mode on
+    add     r6, pc, #1
+    bx  r6
+
+    .code   16
+    # _write()
+    mov     r2, #16
+    mov r1, pc
+    add r1, #12
+    mov     r0, $0x1
+    mov     r7, $0x4
+    svc     0
+
+    # _exit()
+    sub r0, r0, r0
+    mov     r7, $0x1
+    svc 0
+
+.ascii "shell-storm.org\n"
